601903bb1d424c840931d6f8c756ff73bd2f193e
Eva Gunawan <gunawanevaa@gmail.com>
2024-08-07 16:45:07 -0500

    Refactor to be more module like instead of copying and pasting.

--- bin/final_report_evaluation.py
+++ bin/final_report_evaluation.py
@@ -6,7 +6,7 @@ import sys
 
 import pandas as pd
 
-logging.basicConfig(level = logging.DEBUG, format = '%(levelname)s : %(message)s', force = True)
+logging.basicConfig(level = logging.INFO, format = '%(levelname)s : %(message)s', force = True)
 
 def parse_args(args=None):
     Description=('Evaluate viralrecon WSLH report.')
@@ -17,19 +17,18 @@ def parse_args(args=None):
     return parser.parse_args(args)
 
 def evaluate_columns_c_e(report):
+    logging.debug("Evaluating columns C and E.")
 
     df = pd.read_csv(report)
+
     failing_samples = df[pd.isna(df.iloc[:,2])]
     failing_samples = df[pd.isna(df.iloc[:,4])]
     failing_sample_names = failing_samples.iloc[:,0].tolist()
 
-    for sample in failing_sample_names:
-        if "Q" in sample:
-            failing_sample_names.remove(sample)
-
     return df, failing_sample_names
 
 def evaluate_column_l(df, failing_samples_names):
+    logging.debug("Evaluating columns L.")
 
     failing_samples = df[df["depth_after_trimming"] < 100]
     add_failing_samples = failing_samples.iloc[:,0].tolist()
@@ -38,14 +37,48 @@ def evaluate_column_l(df, failing_samples_names):
     return failing_samples_names
 
 def evaluate_column_m(df, failing_samples_names):
-    failing_samples = df[df["depth_after_trimming"] < 100]
+    logging.debug("Evaluating columns M.")
+
+    failing_samples = df[df["1X_coverage_after_trimming"] < 90]
+    add_failing_samples = failing_samples.iloc[:,0].tolist()
+    failing_samples_names.extend(add_failing_samples)
+
+    return failing_samples_names
+
+def evaluate_column_t(df, failing_samples_names):
+    logging.debug("Evaluating columns T.")
+
+    failing_samples = df[df["num_Ns_per_100kb_consensus"] > 10000]
+    add_failing_samples = failing_samples.iloc[:,0].tolist()
+    failing_samples_names.extend(add_failing_samples)
+
+    return failing_samples_names
+
+
+def remove_control(failing_samples_names):
+    logging.debug("Removing control if present.")
+
+    for sample in failing_samples_names:
+        if "Q" in sample:
+            failing_samples_names.remove(sample)
+
+    return failing_samples_names
 
 def main(args=None):
     args = parse_args(args)
 
     logging.info("Beginning to process WSLH report")
+
     report_df, failing_samples = evaluate_columns_c_e(args.wslh_report)
-    evaluate_column_l(report_df, failing_samples)
+    failing_samples = evaluate_column_l(report_df, failing_samples)
+    failing_samples = evaluate_column_m(report_df, failing_samples)
+    failing_samples = evaluate_column_t(report_df, failing_samples)
+    failing_samples = remove_control(failing_samples)
+
+    if len(failing_samples) > 0:
+        logging.info(f"Failing samples are present: {failing_samples}")
+    else:
+        logging.info("There are no failing samples in this report.")
 
 if __name__ == "__main__":
     sys.exit(main())
